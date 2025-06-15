3e49ee8b8778810271b81b91cd42adf72b93e530
Duvan0417 <narvaezduvan29@gmail.com>
2025-06-15 14:00:32 -0500

    cards

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,38 +1,46 @@
-import React from 'react'
-import '../Section/Section.css'
-import userImg from '../../assets/snake.png'
-import watermelonimg from '../../assets/watermelon.png'
-const fruits=['watermelon','apple','blueberry']
-const users=[
+import React from 'react';
+import '../Section/Section.css';
+import userImg from '../../assets/snake.png';
+import watermelonimg from '../../assets/watermelon.png';
+import blueberryimg from '../../assets/blueberry.png';
+
+const fruits = ['watermelon', 'apple', 'blueberry'];
+
+const users = [
     {
-        id:1,
-        name:'apple',
-        descripcion:'apple red',
-        img:'userImg'
+        id: 1,
+        name: 'apple',
+        descripcion: 'apple red',
+        img: userImg
     },
-        {
-        id:2,
-        name:'watermelon',
-        descripcion:'watermelon green',
-        img:'watermelonimg'
+    {
+        id: 2,
+        name: 'watermelon',
+        descripcion: 'watermelon green',
+        img: watermelonimg
+    },    
+    {
+        id: 3,
+        name: 'blueberry',
+        descripcion: 'blueberry',
+        img: blueberryimg
     }
-]
+];
 
 export default function Section() {
-    return(
+    const handleClick = (name) => {
+        console.log(`Contactarle a ${name}`);
+    };
+
+    return (
         <section className='Section'>
-            {
-                users.map(users=>{
-                    return(
-                    <div key={users.id}>
-                        <img src= {userImg} alt={users.name} />
-                        <h2>{users.name}</h2>
-                        <p>{users.descripcion}</p>
-                    </div>
-                    )
-                })
-                
-            }
+            {users.map(({ id, name, descripcion, img }) => (
+                <div className='card' key={id}>
+                    <img className='image' src={img} alt={name} />
+                    <h2 className='h2'>{descripcion}</h2>
+                    <button id={id} onClick={() => handleClick(name)} className='button'>Contactarle</button>
+                </div>
+            ))}
         </section>
-    )
+    );
 }
